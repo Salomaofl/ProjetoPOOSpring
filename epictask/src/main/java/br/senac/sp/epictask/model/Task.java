1156437d0ef1c05436b923d068c3006294aea90e
Wendell Sousa <wendellsousap@gmail.com>
2023-11-17 21:53:06 -0300

    Projeto POO SrpringBoot

---
+++ epictask/src/main/java/br/senac/sp/epictask/model/Task.java
@@ -0,0 +1,29 @@
+package br.senac.sp.epictask.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Entity
+@Data
+public class Task {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @NotBlank
+    String title;
+
+    @Size(min = 10, message = "Digite uma descrição com pelo menos 10 caracteres!")
+    String description;
+
+    @Min(value = 1, message = "A pontuação não pode ser negativa")
+    Integer score;
+    
+}
